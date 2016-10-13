232610f79fed59ffd0fe622baf1443fc1a79e2a5
Jérémie Drouet <jeremie.drouet@gmail.com>
2016-10-13 18:40:18 +0200

    Add constructor with default properties

--- source/index.js
+++ source/index.js
@@ -1,5 +1,16 @@
 export default class DeepFinger {
 
+  static defaultProps = {
+    timeout: 1.0 // timeout to cut words
+  };
+
+  constructor(props = {}) {
+    this.props = {
+      ...DeepFinger.defaultProps,
+      ...props
+    };
+  }
+
   events = [];
   keyListener = event => this.events.push(event);
 
--- test/index.js
+++ test/index.js
@@ -10,6 +10,13 @@ describe('DeepFinger module', function() {
   it('should be instanciable', () => {
     let instance = new DeepFinger();
     expect(instance).to.be.an.instanceOf(DeepFinger);
+    expect(instance.props).to.be.an('object');
+  });
+
+  it('should have default props', () => {
+    let instance = new DeepFinger();
+    expect(instance.props).to.be.an('object');
+    expect(instance.props.timeout).to.eql(DeepFinger.defaultProps.timeout);
   });
 
 });
